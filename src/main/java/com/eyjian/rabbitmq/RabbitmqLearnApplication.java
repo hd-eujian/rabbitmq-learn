db797bc50ccd7a19af5f08b716e5128ef0bbf7e7
yeyongjian <yeyongjian@gac-nio.com>
2019-05-18 13:17:05 +0800

    Initial commit

---
+++ src/main/java/com/eyjian/rabbitmq/RabbitmqLearnApplication.java
@@ -0,0 +1,13 @@
+package com.eyjian.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqLearnApplication.class, args);
+    }
+
+}
